05c553ef77d4e5a630dca3e335834d1c89dffeb4
Nicole Cohen <3121217c@student.gla.ac.uk>
2026-03-02 13:19:46 +0000

    user profile page added, logic updated for nav menu

--- munro_app/views.py
+++ munro_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from munro_app.models import Munro, ClimbRecord, Photo, UserFavouriteMunro
 
 # Create your views here.
 
@@ -6,4 +7,14 @@ def index(request):
     #need further code in here
     #context_dict = []
 
-    return render(request, 'index.html')
+    return render(request, 'munro/index.html')
+
+def user_profile(request):
+    # need to add in our relevant models 
+    
+    # context_dict = {}
+
+    # try: 
+    #     munro = Munro.objects.get()
+
+    return render(request, 'munro/user_profile.html')
--- static/js/main_script.js
+++ static/js/main_script.js
@@ -2,7 +2,6 @@
 //code for menu toggle 
 function menuToggle(){
     var x = document.getElementById('menuLinks');
-
     if (x.style.display === "block"){
         x.style.display = 'none';
     } else {
