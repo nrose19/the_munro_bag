0c3be1f743aca0219c27b614f73923bf177b7ad0
Nicole Cohen <3121217c@student.gla.ac.uk>
2026-03-02 20:01:59 +0000

    fixed MB logo and adjusted home links

--- static/js/main_script.js
+++ static/js/main_script.js
@@ -1,11 +1,21 @@
 
 //code for menu toggle 
 function menuToggle(){
-    var x = document.getElementById('menuLinks');
-    if (x.style.display === "block"){
-        x.style.display = 'none';
+    var menu = document.getElementById('menuLinks');
+    if (menu.style.display === "block"){
+        menu.style.display = 'none';
     } else {
-        x.style.display = "block";
+        menu.style.display = "block";
+    }
+}
+
+//logo colour to change when not on homepage
+function logoColour(){
+    const logo = document.querySelector('.logo');
+    if(logo.id = 'home'){
+        logo.style.borderBottomColor = '#234473'
+    } else {
+        logo.style.borderBottomColor = '#7AACD3'
     }
 }
 
