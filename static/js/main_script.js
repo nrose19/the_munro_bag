5008fd85eff1c52bdb4ddcd4f4fb3d1c01ba102e
Nicole Cohen <3121217c@student.gla.ac.uk>
2026-03-03 19:25:00 +0000

    cherry-pick two

---
+++ static/js/main_script.js
@@ -0,0 +1,12 @@
+
+//code for menu toggle 
+function menuToggle(){
+    var x = document.getElementById('menuLinks');
+
+    if (x.style.display === "block"){
+        x.style.display = 'none';
+    } else {
+        x.style.display = "block";
+    }
+}
+
