c8a434ef3d2c6a1ef9948276c6bb00fa679fe920
Nicole <113370886+nrose19@users.noreply.github.com>
2026-03-19 00:23:03 +0000

    Update settings.py

--- munrobag/settings.py
+++ munrobag/settings.py
@@ -146,6 +146,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
+STATIC_ROOT = STATIC_DIR
 STATIC_URL = '/static/'
 MEDIA_ROOT = MEDIA_DIR
 MEDIA_URL = '/media/'
